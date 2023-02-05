d53a3e1d953ffa24efdf6c618730bfecdb7e26cb
unknown <awaishk640@gmail.com>
2023-02-05 19:14:14 +0530

    Added vercel

--- class_room_app/src/routes/signUp.js
+++ class_room_app/src/routes/signUp.js
@@ -10,7 +10,7 @@ export default function SignUp(){
     async function signUpUser(e){
         
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/api/signUpUser", {
+        const response = await fetch("https://class-room-app-server.vercel.app/api/signUpUser", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
